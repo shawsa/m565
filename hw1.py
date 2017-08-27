bb3962d5cefa3d6a2c6979c99a682de00e2a47c2
sage <sage.b.shaw@gmail.com>
2017-08-27 12:02:00 -0600

    working upload

--- hw1.py
+++ hw1.py
@@ -9,12 +9,8 @@ j = 1;
 while |a − b| ≥ ε do
 y = a;
 a = (a + b)/2;
-b =
-√
-by
-t = t − j(a − y)
-2
-;
+b = sqrt(by)
+t = t − j(a − y)**2
 j = 2j;
 pi_approx = a
 2/t;
@@ -23,4 +19,43 @@ return pi_approx;
 
 '''
 
+from math import sqrt, pi
+from decimal import *
 
+def pi_approx(epsilon):
+	
+	pi_approx = []
+
+	a = 1
+	b = 1/sqrt(2)
+	t = 1/4
+	j = 1
+	
+	while abs(a-b) >= epsilon:
+		y = a
+		a , b = (a+b)/2 , sqrt(b*y)
+		t -= j*(a-y)**2
+		j *= 2
+		pi_approx.append(a**2/t)
+
+	return pi_approx
+
+def pi_approx_dec(digits):
+	getcontext().prec = digits+2
+	epsilon = Decimal(10)**(-1*digits)
+	
+	pi_approx = []
+
+	a = Decimal(1)
+	b = 1 / Decimal(2).sqrt()
+	t = Decimal(1/4)
+	j = Decimal(1)
+	
+	while abs(a-b) >= epsilon:
+		y = a
+		a , b = (a+b)/2 , (b*y).sqrt()
+		t -= j*(a-y)**2
+		j *= 2
+		pi_approx.append(a**2/t)
+
+	return pi_approx
