c517064a65d383a713ad67443dd9c02b7d27925c
sage <sage.b.shaw@gmail.com>
2017-09-19 00:41:01 -0600

    hw2 working commit

--- hw2.py
+++ hw2.py
@@ -93,16 +93,74 @@ def hw2_p3a():
     
 def foo_3(x,a):
     return .5*(x + a/x)
-def ident(x):
-    return x
+def foo_4(x,a):
+    return x/2 * (3-x**2/a)
       
 def hw2_p3b():
-    xs = np.linspace(-.1,3,1000)
+    xs = np.linspace(-.1,10,1000)
     ys = np.vectorize(foo_3)(xs,2)
-    ys_1 = np.vectorize(ident)(xs)
     plt.plot(xs,ys, 'b-')
-    plt.plot(xs,ys_1, 'r-')
-    plt.show()
+    plt.plot(xs,xs, 'r-')
+    plt.xlim(0, 10)
+    plt.ylim(0,10)
+    #plt.show()
     
+    xs = np.linspace(-2,5,1000)
+    ys = np.vectorize(foo_4)(xs,2)
+    plt.plot(xs,ys, 'b-')
+    plt.plot(xs,xs, 'r-')
+    plt.xlim(-2, 5)
+    plt.ylim(-2,5)
+    #plt.show()
+    
+    #check for convergence
+    starts = [.0001, 1, 2, 4, 100]
+    ends = []
+    epsilon = 1e-13
+    for x0 in starts:
+        x = x0
+        for i in range(10**6):
+            x_prev = x
+            x = foo_3(x,2)
+            if abs(x-x_prev) < epsilon:
+                break
+        ends.append(x)
+    latex_table((starts, ends), ('$x_0$', 'converged'))
+    
+    starts = [-100, -10, -5, -1*np.sqrt(10/3)-.001, -1*np.sqrt(10/3)+.001,-1, -.01, .01,1, 1*np.sqrt(10/3)-.001, 1*np.sqrt(10/3)+.001, 1.999,2, 2.001 ,3,3.1, 3.15, 3.2, 3.4, 3.5,3.6,3.7,3.84,5, 10, 100]
+    ends = []
+    for x0 in starts:
+        x = x0
+        for i in range(10**2):
+            x_prev = x
+            x = foo_4(x,2)
+            if abs(x-x_prev) < epsilon:
+                break
+            if abs(x) > 5000:
+                break
+        ends.append(x)
+    latex_table((starts, ends), ('$x_0$', 'converged'))
+    
+def my_inv_root(a, x0, epsilon=1e-15, n_max=10**5):
+    xs = [x0]
+    x = x0
+    for i in range(n_max):
+        if abs(x**2 - a) < epsilon:
+            return xs
+        x = x/2 * (3 - x**2*a)
+        xs.append(x)
+    return  None
+def hw2_p3c():
+    xs = my_inv_root(2, .07)
+    root = 1/np.sqrt(2)
+    err_rel = []
+    err_ratio = ['N/A']
+    ns = []
+    for i, x in enumerate(xs):
+        err_rel.append(abs(x - root))
+        if i != 0:
+            err_ratio.append(np.log(err_rel[i])/np.log(err_rel[i-1]))
+        ns.append(i+1)
+    latex_table((ns, xs, err_rel, err_ratio), ("$n$", "$x_n$", "abs err", 'ratio'))
     
     
